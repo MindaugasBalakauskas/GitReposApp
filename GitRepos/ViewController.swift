68a2acff3179a17479e8721882333ff511975bb8
Mindaugas B <mindaugas.balakauskas@gmail.com>
2022-10-17 10:33:58 +0100

    Initial commit

---
+++ GitRepos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitRepos
+//
+//  Created by Mindaugas Balakauskas on 17/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
